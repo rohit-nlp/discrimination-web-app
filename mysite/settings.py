9d9ae7feaa8c3dbf956b00c6e5ad759c49b41cb4
Blaieet <blrasjim7@alumnes.ub.edu>
2020-01-17 19:13:27 +0100

    this is for sure the right allowed hosts list

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = '3w5+j6wh0^w9pjv14egctmtb^(7cq#whl%q(_8t*_(q2y^5z@a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-34-225-210-97.compute-1.amazonaws.com:8000/','127.0.0.1']
+ALLOWED_HOSTS = ['ec2-34-225-210-97.compute-1.amazonaws.com','ec2-34-225-210-97.compute-1.amazonaws.com:8000/','127.0.0.1']
 
 
 # Application definition
